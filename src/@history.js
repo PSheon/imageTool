ee4f994d1331a89273484ff41e52a5f36d945591
Paul Jiang <paul.jiang@manpowergrc.tw>
2019-11-22 20:52:20 +0800

    [更新] homepage 路由

--- src/@history.js
+++ src/@history.js
@@ -1,7 +1,7 @@
 import * as history from 'history';
 import ReactGA from 'react-ga';
 
-const HISTORY = history.createBrowserHistory();
+const HISTORY = history.createBrowserHistory({ basename: '/imageTool' });
 
 HISTORY.listen(location => {
   ReactGA.set({ page: location.pathname });
